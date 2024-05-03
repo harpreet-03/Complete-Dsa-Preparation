ed926f0c569df09c3929df42a4de9fbb5b2d6610
Harpreet singh <harpreet162004@gmail.com>
2024-05-03 19:34:17 +0530

    Stacks

--- Stacks/Stack_using_JCF.java
+++ Stacks/Stack_using_JCF.java
@@ -4,18 +4,26 @@ public class Stack_using_JCF {
     public static void main(String[] args) {
         // Create a stack using JCF (Java Collections Framework)
         Stack<Integer> s = new Stack<Integer>();
-        
+
         s.push(1);
         s.push(2);
         s.push(3);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
-        
-        
+        int elementToSearch = 2;
+        boolean found = false;
 
+        Iterator<Integer> iterator = s.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() == elementToSearch) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element " + elementToSearch + " found in the stack.");
+        } else {
+            System.out.println("Element " + elementToSearch + " not found in the stack.");
+        }
     }
-    
 }
