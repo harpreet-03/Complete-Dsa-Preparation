77e3e8f56de2f3782dad008dfcff9f8433346f71
Harpreet singh <harpreet162004@gmail.com>
2024-06-14 19:47:01 +0530

    LinkedHashSet in Java

---
+++ Hashing/LinkedHashSet/code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class code {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("India");
+        lhs.add("Australia");
+        lhs.add("South Africa");
+        lhs.add("India");
+        lhs.add("Australia");
+        lhs.add("India");
+
+        System.out.println(lhs); // [India, Australia, South Africa]
+    }
+}
